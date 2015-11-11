0b462748fffd744e3213af0cfa4b3681e39d8e60
Giacomo Donati <donatigiacomo91@gmail.com>
2015-11-11 13:57:48 +0100

    implement commands

--- src/TDP/DieCommand.java
+++ src/TDP/DieCommand.java
@@ -1,4 +1,13 @@
 package TDP;
 
-public class DieCommand {
+public class DieCommand extends LifeCommand {
+
+    public DieCommand(Cell cell) {
+        super(cell);
+    }
+
+    @Override
+    public void execute() {
+        target.die();
+    }
 }
